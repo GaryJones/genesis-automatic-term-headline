a237565ee23d594e13c6c4382390c65dd39bd129
Gary Jones <gary@gamajo.com>
2015-11-08 17:26:56 +0000

    Code tidy

--- genesis-automatic-term-headline.php
+++ genesis-automatic-term-headline.php
@@ -2,11 +2,11 @@
 /**
  * Genesis Automatic Term Headline
  *
- * @package   GenesisAutomaticTermHeadline
- * @author    Gary Jones <gary@garyjones.co.uk>
- * @license   GPL-2.0+
- * @link      https://github.com/GaryJones/genesis-automatic-term-headline
- * @copyright 2013 Gary Jones, Gamajo Tech
+ * @package           Genesis_Automatic_Term_Headline
+ * @author            Gary Jones
+ * @license           GPL-2.0+
+ * @link              https://github.com/GaryJones/genesis-automatic-term-headline
+ * @copyright         2013 Gamajo Tech
  *
  * @wordpress-plugin
  * Plugin Name:       Genesis Automatic Term Headline
@@ -14,11 +14,11 @@
  * Description:       Automatically adds a headline to the term archive page, the same as the name of taxonomy term, if no explicit value is given.
  * Version:           1.1.1
  * Author:            Gary Jones
- * Author URI:        http://gamajo.com/
+ * Author URI:        https://gamajo.com/
  * Text Domain:       genesis-automatic-term-headline
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Domain Path:       /lang
+ * Domain Path:       /languages
  * GitHub Plugin URI: https://github.com/GaryJones/genesis-automatic-term-headline
  * GitHub Branch:     master
  */
@@ -42,11 +42,9 @@ add_action( 'genesis_before_loop', 'genesis_automatic_term_headline_do_taxonomy_
 /**
  * Change the behaviour of the default term headline, by making the default the name of the term.
  *
- * If the headline has been customised in some way, hen this will show instead.
+ * If the headline has been customised in some way, then this will show instead.
  *
  * @since 1.0.0
- *
- * @author Gary Jones, Gamajo Tech
  *
  * @return null Return early if not the correct archive page, not page one, or no term meta is set.
  */
@@ -54,28 +52,32 @@ function genesis_automatic_term_headline_do_taxonomy_title_description() {
 
 	global $wp_query;
 
-	if ( ! is_category() && ! is_tag() && ! is_tax() )
+	if ( ! is_category() && ! is_tag() && ! is_tax() ) {
 		return;
+	}
 
-	if ( get_query_var( 'paged' ) >= 2 )
+	if ( get_query_var( 'paged' ) >= 2 ) {
 		return;
+	}
 
 	$term = is_tax() ? get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) ) : $wp_query->get_queried_object();
 
-	if ( ! $term || ! isset( $term->meta ) )
+	if ( ! $term || ! isset( $term->meta ) ) {
 		return;
+	}
 
-	if ( $term->meta['headline'] )
+	$headline_text = $term->name;
+
+	if ( $term->meta['headline'] ) {
 		$headline_text = $term->meta['headline'];
-	elseif ( apply_filters( 'genesis-automatic-term-headline-exclusion', false ) )
+	} elseif ( apply_filters( 'genesis-automatic-term-headline-exclusion', false ) ) {
 		$headline_text = '';
-	else
-		$headline_text = $term->name;
+	}
 
 	$headline = sprintf( '<h1 class="archive-title">%s</h1>', strip_tags( $headline_text ) );
 
 	$intro_text = $term->meta['intro_text'] ? apply_filters( 'genesis_term_intro_text_output', $term->meta['intro_text'] ) : '';
-
-	printf( '<div class="archive-description taxonomy-description">%s</div>', $headline . $intro_text );
-
+	?>
+	<div class="archive-description taxonomy-description"><?php echo $headline . $intro_text; ?></div>
+	<?php
 }
